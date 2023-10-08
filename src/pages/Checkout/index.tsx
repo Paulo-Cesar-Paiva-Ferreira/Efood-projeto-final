efec76e1f5c81f366b56e43f62896975891ffe2b
Paulo César Paiva Ferreira <pcgms7@hotmail.com>
2023-10-08 10:38:21 -0300

    finalizando projeto

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -636,7 +636,7 @@ const Checkout = () => {
                     <div>
                       <label htmlFor="cep">CEP</label>
                       <InputMask
-                        mask="999.999.999-99"
+                        mask="99999.999"
                         type="text"
                         id="cep"
                         name="cep"
